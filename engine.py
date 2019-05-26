f73a83da71fdf66486e8d6f2870fbb451511d512
smithdamen <smith.damen@gmail.com>
2019-05-26 02:36:48 -0400

    edit directory

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
 # IMPORT STATEMENTS
 print('# running imports')
 import tcod as libtcod
+
+from src.input_handlers import handle_keys
 print('# imports completed')
 
 # MAIN GAME LOOP
@@ -25,22 +27,39 @@ def main():
     # initialize root console
     libtcod.console_init_root(screen_width, screen_height, 'pyrl', False)
 
+    con = libtcod.console_new(screen_width, screen_height)
+
     # game loop
     while not libtcod.console_is_window_closed():
         # listen for input
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         # set variables for root console (console printing to, color)
-        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_set_default_foreground(con, libtcod.white)
         # (console printing to, x coord, y coord, symbol to use, background color)
-        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         # draws everything to screen
         libtcod.console_flush()
+        # puts an empty space where the player used to be
+        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
 
         # listen for keypresses
-        key = libtcod.console_check_for_keypress()
+        action = handle_keys(key)
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
 
-        if key.vk == libtcod.KEY_ESCAPE:
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
             return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 if __name__ == '__main__':
     main()
